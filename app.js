1ea22ca3748bfeb3242b5b95976a4f2e31e2eb91
saki1994 <67071604+saki1994@users.noreply.github.com>
2021-02-14 20:48:43 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require("ejs");
+
+const app = express();
+const port = 3000;
+
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+})
+app.listen(port, () =>{
+  console.log("Server is open in port 3000.")
+})
