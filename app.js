3a71f9a0b32681992e95d407fa958ed8324a052a
saki1994 <67071604+saki1994@users.noreply.github.com>
2021-03-13 21:54:41 +0100

    Categories content added -unfinished

--- app.js
+++ app.js
@@ -11,11 +11,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 const pageTitles = ["Tin-Recipes", "Categories", "About Page"]
-
+const qoutes = [
+  "Recipes to inspire your inner chef",
+  "People who love to cook are always the best people",
+  "Our Mission: is to stand for something good and healthy"
+]
 app.get("/", (req, res) => {
 
   res.render("home", {
-    title: pageTitles[0]
+    title: pageTitles[0],
+    webQoute: qoutes[0]
   });
 
 });
@@ -24,7 +29,8 @@ app.get("/categories", (req, res) => {
   const homepage = "Categories";
 
   res.render("categories", {
-    title: pageTitles[1]
+    title: pageTitles[1],
+    webQoute: qoutes[1]
   });
 
 });
@@ -33,7 +39,8 @@ app.get("/about", (req, res) => {
   const homepage = "About Page";
 
   res.render("about", {
-    title: pageTitles[2]
+    title: pageTitles[2],
+    webQoute: qoutes[2]
   });
 
 
