55145e353004bcb8b67f16636d26d882cad09e02
saki1994 <67071604+saki1994@users.noreply.github.com>
2021-03-18 11:38:15 +0100

    saved cat

--- public/main.js
+++ public/main.js
@@ -1,3 +1,49 @@
 
-let categoriesBtn = document.querySelectorAll(".categories-btn");
+let categoriesBtn = document.querySelectorAll("p.categories-btn");
 let sectionDiv = document.querySelectorAll(".section");
+
+
+
+
+function looping(number1) {
+  for (let i=0; i<sectionDiv.length; i++) {
+
+       if (i === number1) {
+         sectionDiv[i].style.visibility = "visible";
+       } else {
+       sectionDiv[i].style.visibility = "hidden";
+       }
+      }
+}
+
+function clicked(buttonClicked, divNumber) {
+
+  const txt = buttonClicked.innerHTML;
+
+  switch (txt) {
+    case "Chicken":
+       looping(divNumber)
+      break
+    case "Beef":
+      looping(divNumber)
+      break
+    case "Pork":
+      looping(divNumber)
+      break
+      case "Veggies":
+      looping(divNumber)
+      break
+       case "Other Meat":
+      looping(divNumber)
+      break
+      case "desserts-section":
+     looping(divNumber)
+     break
+  }
+};
+
+for (let i = 0; i < categoriesBtn.length; i++) {
+  categoriesBtn[i].addEventListener("click", () =>
+    clicked(categoriesBtn[i],i)
+  )
+}
